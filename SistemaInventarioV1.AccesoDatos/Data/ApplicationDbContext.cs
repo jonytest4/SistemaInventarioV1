24c841ec6ba2691b4e45ed0b377bf9c92eeb4569
jonytest4 <jjjt01@hotmail.com>
2023-07-08 13:22:04 -0500

    Crear Proyecto

---
+++ SistemaInventarioV1.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+//referenciar correctamente el namespace en este caso se referencia al proyecto AccesoDatos **//
+namespace SistemaInventarioV1.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SistemaInventarioV1.Utilidades/DS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ //DS (definiciones estáticas)
+namespace SistemaInventarioV1.Utilidades
+{
+    public static class DS
+    {
+    }
+}
